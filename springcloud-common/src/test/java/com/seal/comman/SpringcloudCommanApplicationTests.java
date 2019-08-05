920f553f617e517ad539718b4c1b5022964ef4d6
youngMen1 <fengzhiqiangzxx199644>
2019-08-05 11:39:40 +0800

    desc:版本升级

--- springcloud-common/src/test/java/com/seal/comman/SpringcloudCommanApplicationTests.java
+++ springcloud-common/src/test/java/com/seal/comman/SpringcloudCommanApplicationTests.java
@@ -5,12 +5,34 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringcloudCommanApplicationTests {
 
     @Test
     public void contextLoads() {
+        File file = new File(getClass().getResource("E:/text.txt").getFile());
+        BufferedReader reader;
+        String text = "";
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while (true) {
+                line = reader.readLine();
+                if (line == null) {
+                    break;
+                }
+                text += line.trim() + "\n";
+            }
+            reader.close();
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(text);
     }
 
 }
