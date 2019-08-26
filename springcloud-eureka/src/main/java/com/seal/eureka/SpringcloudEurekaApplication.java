0b4ef23f231030cc42a54c090c09706fefc9e366
youngMen1 <fengzhiqiangzxx199644>
2019-08-26 17:28:24 +0800

    desc:eureka

--- springcloud-eureka/src/main/java/com/seal/eureka/SpringcloudEurekaApplication.java
+++ springcloud-eureka/src/main/java/com/seal/eureka/SpringcloudEurekaApplication.java
@@ -2,7 +2,7 @@ package com.seal.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -12,7 +12,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @description 高可用注册中心
  **/
 @EnableEurekaServer
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringcloudEurekaApplication {
 
